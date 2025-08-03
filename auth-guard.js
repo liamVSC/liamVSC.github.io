bdda29c42be3389b6050d7095b61e4982a399954
liamVSC <43729866+liamVSC@users.noreply.github.com>
2025-08-03 23:05:38 +0100

    update

--- auth-guard.js
+++
@@ -1,8 +0,0 @@
-import { auth } from './firebase-config.js';
-import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js';
-
-onAuthStateChanged(auth, (user) => {
-  if (!user) {
-    window.location.href = 'login.html';
-  }
-});
