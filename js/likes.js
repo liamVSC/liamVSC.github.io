3a907989f533c5668edbb27b47231e8de26ca0f9
liamVSC <43729866+liamVSC@users.noreply.github.com>
2025-08-04 03:31:57 +0100

    c

--- js/likes.js
+++
@@ -1,8 +0,0 @@
-async function likeScript(scriptId) {
-    const scriptRef = db.collection("scripts").doc(scriptId);
-    await db.runTransaction(async (transaction) => {
-        const doc = await transaction.get(scriptRef);
-        const newLikes = (doc.data().likes || 0) + 1;
-        transaction.update(scriptRef, { likes: newLikes });
-    });
-}
